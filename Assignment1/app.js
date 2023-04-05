ec9a0a4fcee2eb8bdb04b3411b834e8fa322487f
ahmed-hamza <ahmedarif2882@gmail.com>
2023-04-05 23:52:50 +0500

    Assignment 1 completed

---
+++ Assignment1/app.js
@@ -0,0 +1,15 @@
+// Assignment #1
+let userAge = prompt("What is your Age?")
+if (userAge <= 10){
+    console.log("You're Welcome")
+} else {
+    console.log("You're not Allowed")
+}
+
+// Assignment #2
+let num = prompt("Enter Number")
+
+for (i=1; i <= 10; i++){
+    let table = num + " x " + i + " = " + num*i
+    console.log(table)
+}
